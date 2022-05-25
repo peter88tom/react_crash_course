cda3707718e4887439ce83a263c80976139459b8
peter88tom <peter88tom@gmail.com>
2022-05-25 04:19:51 +0200

    Using operators in DOM

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@
 function App() {
   const name = "Code"
 
+  // Operator
+  const x = true
+
   return (
     <div className="App">
       <h1>Hello {name}</h1>
+      <h1>Hello {x ? 'Yes': 'No'}</h1>
     </div>
   );
 }
