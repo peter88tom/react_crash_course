daef376cf0d1829c529b09852d1c3c94f1de9297
peter88tom <peter88tom@gmail.com>
2022-05-25 04:41:54 +0200

    Add header component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
+import Header from "./components/Header";
 
 function App() {
-  const name = "Code"
-
-  // Operator
-  const x = true
-
   return (
     <div className="App">
-      <h1>Hello {name}</h1>
-      <h1>Hello {x ? 'Yes': 'No'}</h1>
+      <Header />
     </div>
   );
 }
