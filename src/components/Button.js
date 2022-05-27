71357011e3056034a8b185495c7f7a6b26e868e2
peter88tom <peter88tom@gmail.com>
2022-05-27 03:39:23 +0200

    Add a click function on button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,19 @@
 import PropTypes from 'prop-types'
 
 const Button = ({color, text}) => {
-	return <button style={{ backgroundColor: color}} className='btn'>{text}</button>
+	// Onclick button
+	const onClick = () => {
+		console.log('Clicked')
+	}
+
+
+	return <button 
+		onClick={onClick} 
+		style={{ backgroundColor: color}} 
+		className='btn'
+		>
+		{text}
+		</button>
 }
 
 Button.defaultProps = {
