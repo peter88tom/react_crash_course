391afa980c2265b731492ed1db9767d0f752c327
peter88tom <peter88tom@gmail.com>
2022-05-27 02:57:37 +0200

    Make Add button to be a separate component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
+import Button from './Button'
+
 const Header = (props) => {
 	return (
 		<header className='header'>
 			<h1>{props.title}</h1>
-			<button className='btn'>Add</button>
+			<Button />
 		</header>
 	)
 }
