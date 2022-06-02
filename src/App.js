ebd24ac1f37549a85a90d326ad9b6d2c73b64dfc
peter88tom <peter88tom@gmail.com>
2022-06-02 03:23:16 +0200

    Move task list to App global level

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
+import { useState } from 'react'
 import Header from "./components/Header"
 import Tasks from "./components/Tasks";
 
 function App() {
+
+  // Make tasks list to be part of the componet
+	// If we want to change part of the component we can use setTasks
+	// state is immutable you can not change
+	const [tasks, setTaks] = useState(
+		[
+			{
+				id: 1,
+				text: 'Doctors Appointment',
+				day: 'Feb 5th at 2:30pm',
+				reminder: true,
+			},
+			{
+				id: 2,
+				text: 'Meeting at School ',
+				day: 'Feb 6th at 1:30pm',
+				reminder: true,
+			},
+			{
+				id: 3,
+				text: 'Food Shopping',
+				day: 'Feb 5th at 4:30pm',
+				reminder: false,
+			}
+		]
+	)
+
+
   return (
     <div className="App">
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
