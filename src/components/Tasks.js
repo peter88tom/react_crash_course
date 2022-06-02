77699f5d66ab958d9fe5f23085115ad60153c603
peter88tom <peter88tom@gmail.com>
2022-06-02 03:20:18 +0200

    useState in Task component

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,25 +1,32 @@
-const tasks = [
-	{
-		id: 1,
-		text: 'Doctors Appointment',
-		day: 'Feb 5th at 2:30pm',
-		reminder: true,
-	},
-	{
-		id: 2,
-		text: 'Meeting at School',
-		day: 'Feb 6th at 1:30pm',
-		reminder: true,
-	},
-	{
-		id: 3,
-		text: 'Food Shopping',
-		day: 'Feb 5th at 4:30pm',
-		reminder: false,
-	}
-]
+import { useState } from 'react'
 
 const Tasks = () =>{
+	// Make tasks list to be part of the componet
+	// If we want to change part of the component we can use setTasks
+	// state is immutable you can not change
+	const [tasks, setTaks] = useState(
+		[
+			{
+				id: 1,
+				text: 'Doctors Appointment',
+				day: 'Feb 5th at 2:30pm',
+				reminder: true,
+			},
+			{
+				id: 2,
+				text: 'Meeting at School ',
+				day: 'Feb 6th at 1:30pm',
+				reminder: true,
+			},
+			{
+				id: 3,
+				text: 'Food Shopping',
+				day: 'Feb 5th at 4:30pm',
+				reminder: false,
+			}
+		]
+	)
+
 	return (
 		<>
 		{tasks.map((task) => (
